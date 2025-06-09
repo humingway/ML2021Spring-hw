1917da8662d448bfc18abc2d87fb63dd0d203890
humingway <1040358185@qq.com>
2025-06-09 21:29:51 +0800

    HW2	Classification 仅跑通无修改 数据集太大仅提供种子文件

---
+++ hw2/hw2.py
@@ -0,0 +1,208 @@
+import numpy as np
+
+print('Loading data ...')
+
+# 从.npy 文件（NumPy 数组）加载训练和测试数据。
+data_root='./hw2/timit_11/'
+train = np.load(data_root + 'train_11.npy')
+train_label = np.load(data_root + 'train_label_11.npy')
+test = np.load(data_root + 'test_11.npy')
+
+print('Size of training data: {}'.format(train.shape))
+print('Size of testing data: {}'.format(test.shape))
+
+# 创建 Dataset
+import torch
+from torch.utils.data import Dataset
+
+class TIMITDataset(Dataset):
+    def __init__(self, X, y=None):
+        self.data = torch.from_numpy(X).float()
+        if y is not None:
+            y = y.astype(int)
+            self.label = torch.LongTensor(y)
+        else:
+            self.label = None
+
+    def __getitem__(self, idx):
+        if self.label is not None:
+            return self.data[idx], self.label[idx]
+        else:
+            return self.data[idx]
+
+    def __len__(self):
+        return len(self.data)
+    
+    # 将标记数据分成训练集和验证集，可以修改变量VAL_RATIO来改变验证数据的比例。
+VAL_RATIO = 0.2
+
+percent = int(train.shape[0] * (1 - VAL_RATIO))
+train_x, train_y, val_x, val_y = train[:percent], train_label[:percent], train[percent:], train_label[percent:]
+print('Size of training set: {}'.format(train_x.shape))
+print('Size of validation set: {}'.format(val_x.shape))
+
+# 从数据集创建一个数据加载器，可以随意调整这里的变量 BATCH_SIZE。
+BATCH_SIZE = 64
+
+from torch.utils.data import DataLoader
+
+train_set = TIMITDataset(train_x, train_y)
+val_set = TIMITDataset(val_x, val_y)
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True) #only shuffle the training data
+val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+
+# 清理不需要的变量以节省内存。
+
+# 注意：如果您稍后需要使用这些变量，则可以删除此块或稍后清理不需要的变量。
+# 数据量非常大，因此请注意 Colab 中的内存使用情况。
+import gc
+
+del train, train_label, train_x, train_y, val_x, val_y
+gc.collect()
+
+# 定义模型架构，鼓励您改变和试验模型架构。
+import torch
+import torch.nn as nn
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.layer1 = nn.Linear(429, 1024)
+        self.layer2 = nn.Linear(1024, 512)
+        self.layer3 = nn.Linear(512, 128)
+        self.out = nn.Linear(128, 39) 
+
+        self.act_fn = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.act_fn(x)
+
+        x = self.layer2(x)
+        x = self.act_fn(x)
+
+        x = self.layer3(x)
+        x = self.act_fn(x)
+
+        x = self.out(x)
+        
+        return x
+
+# 训练
+## 检查显卡
+def get_device():
+  return 'cuda' if torch.cuda.is_available() else 'cpu'
+## 确定随机种子以实现可重复性。
+# fix random seed
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  
+    np.random.seed(seed)  
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+# 请随意在此更改训练参数。
+# fix random seed for reproducibility
+same_seeds(0)
+
+# get device 
+device = get_device()
+print(f'DEVICE: {device}')
+
+# training parameters
+num_epoch = 1               # number of training epoch
+learning_rate = 0.0001       # learning rate
+
+# the path where checkpoint saved
+model_path = './hw2/model.ckpt'
+
+# create model, define a loss function, and optimizer
+model = Classifier().to(device)
+criterion = nn.CrossEntropyLoss() 
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# start training
+
+best_acc = 0.0
+for epoch in range(num_epoch):
+    train_acc = 0.0
+    train_loss = 0.0
+    val_acc = 0.0
+    val_loss = 0.0
+
+    # training
+    model.train() # set the model to training mode
+    for i, data in enumerate(train_loader):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad() 
+        outputs = model(inputs) 
+        batch_loss = criterion(outputs, labels)
+        _, train_pred = torch.max(outputs, 1) # get the index of the class with the highest probability
+        batch_loss.backward() 
+        optimizer.step() 
+
+        train_acc += (train_pred.cpu() == labels.cpu()).sum().item()
+        train_loss += batch_loss.item()
+
+    # validation
+    if len(val_set) > 0:
+        model.eval() # set the model to evaluation mode
+        with torch.no_grad():
+            for i, data in enumerate(val_loader):
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                batch_loss = criterion(outputs, labels) 
+                _, val_pred = torch.max(outputs, 1) 
+            
+                val_acc += (val_pred.cpu() == labels.cpu()).sum().item() # get the index of the class with the highest probability
+                val_loss += batch_loss.item()
+
+            print('[{:03d}/{:03d}] Train Acc: {:3.6f} Loss: {:3.6f} | Val Acc: {:3.6f} loss: {:3.6f}'.format(
+                epoch + 1, num_epoch, train_acc/len(train_set), train_loss/len(train_loader), val_acc/len(val_set), val_loss/len(val_loader)
+            ))
+
+            # if the model improves, save a checkpoint at this epoch
+            if val_acc > best_acc:
+                best_acc = val_acc
+                torch.save(model.state_dict(), model_path)
+                print('saving model with acc {:.3f}'.format(best_acc/len(val_set)))
+    else:
+        print('[{:03d}/{:03d}] Train Acc: {:3.6f} Loss: {:3.6f}'.format(
+            epoch + 1, num_epoch, train_acc/len(train_set), train_loss/len(train_loader)
+        ))
+
+# if not validating, save the last epoch
+if len(val_set) == 0:
+    torch.save(model.state_dict(), model_path)
+    print('saving model at last epoch')
+
+# 创建测试数据集，并从保存的检查点加载模型。
+# create testing dataset
+test_set = TIMITDataset(test, None)
+test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+
+# create model and load weights from checkpoint
+model = Classifier().to(device)
+model.load_state_dict(torch.load(model_path))
+
+# 作出预测
+predict = []
+model.eval() # set the model to evaluation mode
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        inputs = data
+        inputs = inputs.to(device)
+        outputs = model(inputs)
+        _, test_pred = torch.max(outputs, 1) # get the index of the class with the highest probability
+
+        for y in test_pred.cpu().numpy():
+            predict.append(y)
+
+# 将预测结果写入 CSV 文件。
+# 运行完此模块后，从左侧的文件部分下载文件“prediction.csv”，并将其提交至 Kaggle。
+with open('./hw2/prediction.csv', 'w') as f:
+    f.write('Id,Class\n')
+    for i, y in enumerate(predict):
+        f.write('{},{}\n'.format(i, y))
